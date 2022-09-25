f73c14cbbfcd766e7ab6db24a2d63bbd957da388
tim <mit.brooks@gmail.com>
2022-09-25 21:41:16 +0100

    Use gpiozero and code tidy up

--- relay.py
+++ relay.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import wiringpi2
+from gpiozero import LED  # Relay can look like an LED ;-)
 import sys
 import argparse
-a=24 # relay A 
-b=25 # relay B
-s=11 # SPI 
-wiringpi2.wiringPiSetupGpio()
-wiringpi2.pinMode(a, 1)
-wiringpi2.pinMode(b, 1)
-wiringpi2.pinMode(s, 1)
+a = 24  # relay A
+b = 25  # relay B
+s = 11  # SPI
+
+
 def relay(position='not_defined', pin=a):
-  if (position != 'not_defined'):
-    if (position == True or position == 'closed' or int(position) == 1):
-        wiringpi2.digitalWrite(pin, 1)
-    elif(position == False or position == 'open' or int(position) == 0):
-        wiringpi2.digitalWrite(pin, 0)
-    else:
-      print('unknown position {}'.format(position))
-  return wiringpi2.digitalRead(pin)
+    rly = LED(pin)
+    if position != 'not_defined':
+        if position or position == 'closed' or int(position) == 1:
+            rly.on()
+        elif not position or position == 'open' or int(position) == 0:
+            rly.off()
+        else:
+            print('unknown position {}'.format(position))
+    return rly.value
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument('--relay', '-r', default='1', help='the relay number - 25=back hall, 24=main hall ')
-  parser.add_argument('--position', '-p', default= 'not_defined', help='send interger 1 or "close" or boolean True to close the relay')
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--relay', '-r', default='1', help='the relay number - 25=back hall, 24=main hall ')
+    parser.add_argument('--position', '-p', default='not_defined',
+                        help='send integer 1 or "close" or boolean True to close the relay')
 
-  flags=parser.parse_args(sys.argv[1:])
-  print(relay(position=flags.position,pin=int(flags.relay)))
+    flags=parser.parse_args(sys.argv[1:])
+    print(relay(position=flags.position, pin=int(flags.relay)))
 #  if len(sys.argv) == 2:
 #    #print(sys.argv[1], type(sys.argv[1]))
 #    print(relay(position=sys.argv[1]))
