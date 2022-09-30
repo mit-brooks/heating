2af65633f159f27691ede22537238cf07e0abc07
tim <mit.brooks@gmail.com>
2022-09-30 16:41:20 +0100

    time delta aware

--- heating.py
+++ heating.py
@@ -242,7 +242,7 @@ class GCalCron:
         if self.settings['last_sync']:
             temp = dateutil.parser.parse(self.settings['last_sync'])
             # and the last time was more than num_days ago (because that would only get events in the past...)
-            if temp > datetime.datetime.now() - num_days:
+            if temp - datetime.timedelta(days=0) > datetime.datetime.now() - num_days:
                 last_sync = temp
 
         sync_start = datetime.datetime.now(gettz())
